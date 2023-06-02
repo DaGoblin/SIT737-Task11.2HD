166ed3f42d153111bd317e54a45d44e641cedec7
DaGoblin <s222574652@deakin.edu.au>
2023-06-02 17:39:14 +1000

    version v4.0.1

--- AuthService/logger.js
+++ AuthService/logger.js
@@ -1,5 +1,26 @@
 const { createLogger, format, transports } = require("winston");
-
+// const logger = createLogger({
+//     level: "info",
+//     format: format.combine(
+//         format.timestamp({ format: "MMM-DD-YYYY HH:mm:ss" }),
+//         format.align(),
+//         format.printf(
+//             (info) => `${info.level}: ${[info.timestamp]}: ${info.message}`
+//         )
+//     ),
+//     defaultMeta: { service: "Auth-service" },
+//     transports: [
+//         //
+//         // - Write all logs with importance level of `error` or less to `error.log`
+//         // - Write all logs with importance level of `info` or less to `combined.log`
+//         //
+//         new transports.File({
+//             filename: "/logs/error.log",
+//             level: "error",
+//         }),
+//         new transports.File({ filename: "/logs/server.log" }),
+//     ],
+// });
 const logger = createLogger({
     transports: new transports.File({
         filename: "logs/server.log",
